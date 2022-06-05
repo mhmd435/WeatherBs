8e507a193fa705d9f2598dfbed4b9347515f838d
mhmdaghajani <mhm.aghajani435@gmail.com>
2022-06-05 23:35:21 +0430

    refactor codes to BLoc arch and bloc package

--- lib/main.dart
+++ lib/main.dart
@@ -19,38 +19,3 @@ void main() {
   ));
 }
 
-class Error extends StatelessWidget {
-  final String errorMessage;
-  final Function()? onRetryPressed;
-
-  const Error({Key? key, required this.errorMessage, required this.onRetryPressed})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            errorMessage,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-            ),
-          ),
-          SizedBox(height: 8),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(primary: Colors.blueAccent),
-            child: Text('Retry', style: TextStyle(color: Colors.black)),
-            onPressed: onRetryPressed,
-          )
-        ],
-      ),
-    );
-  }
-
-}
-
-
